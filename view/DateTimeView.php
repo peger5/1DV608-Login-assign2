ebe18b352157b31987df074f9b3ba9a282128c60
peger5 <petko.gerdjikov@gmail.com>
2015-09-22 15:11:39 +0200

    fixed datetimeview

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -5,7 +5,7 @@ class DateTimeView {
 
 	public function show() {
 		
-		$timeString = date("l") . ', the '. date("j") . 'th of ' . date("F") . ' ' . date("o") . ', The time is ' . date("G:i");
+		$timeString = date("l") . ', the '. date("jS") . ' of ' . date("F") . ' ' . date("o") . ', The time is ' . date("G:i");
 		
 		return '<p>' . $timeString . '</p>';
 	}
